58450f19460253f81b298d600f16577548be972a
Sithu-BinaryLab <sithulwin.binarylab@gmail.com>
2024-09-16 00:49:19 +0630

    Updated: product detail page and loading

---
+++ src/components/molecules/related-products.tsx
@@ -0,0 +1,75 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import useFetchGetInCategories from "@/app/api/hooks/categories/useFetchGetInCategoires";
+import { Button } from "@/components/atoms/button";
+
+interface RelatedProductsProps {
+  category?: string;
+}
+const RelatedProducts = ({ category }: RelatedProductsProps) => {
+  let currentCategoryLink = "";
+  switch (category) {
+    case "men's clothing":
+      currentCategoryLink = "men's%20clothing";
+      break;
+    case "women's clothing":
+      currentCategoryLink = "women's%20clothing";
+      break;
+    case "jewelery":
+      currentCategoryLink = "jewelery";
+      break;
+    case "electronics":
+      currentCategoryLink = "electronics";
+      break;
+  }
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useFetchGetInCategories(currentCategoryLink);
+
+  const router = useRouter();
+  const handleClick = (id: number) => {
+    if (id) {
+      router.push(`/product/${id}`);
+    }
+    return;
+  };
+
+  return (
+    <div className="my-10">
+      <h1 className="text-2xl text-surface-foreground font-bold mb-2 ">
+        Related Products
+      </h1>
+      <div className="w-full overflow-x-scroll max-w-[500rem] flex gap-x-4 mt-4">
+        {!isLoading &&
+          products?.map((item: any) => (
+            <div
+              key={item.id}
+              className="cursor-pointer lg:min-w-[400px] min-w-[300px] relative"
+              onClick={() => handleClick(item.id)}
+            >
+              <div className="flex relative justify-center py-4 h-[220px]  gap-4 w-full aspect-square   bg-stone-50 rounded-lg border hover:border-blue-600 dark:border-neutral-800">
+                <Image
+                  alt=""
+                  src={item.image}
+                  width={200}
+                  height={500}
+                  className="object-contain transition-all ease-in-out hover:scale-105 duration-300"
+                />
+                <div className="absolute bottom-4 left-4 flex items-center gap-2 rounded-full border border-slate-300 bg-white/70 py-2 px-4 text-xs font-semibold text-black backdrop-blur-md">
+                  <h1 className="text-sm line-clamp-1">{item.title}</h1>
+                  <Button className="shadow-sm px-2 rounded-full text-[13px] font-bold">
+                    ${item.price}
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProducts;
---
+++ src/components/organisms/single-loading.tsx
@@ -0,0 +1,7 @@
+function SingleLoading() {
+  return (
+    <div className="border bg-stone-50 animate-pulse shadow rounded-md p-4 max-w-sm w-full mx-auto w-[250px] h-[335px] opacity"></div>
+  );
+}
+
+export default SingleLoading;
